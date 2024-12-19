5f1efad19d4b62ee708e943f9575ea9d52a14669
Sheikh Jabed <jabedbd2295@gmail.com>
2024-12-19 23:19:18 +0600

    Token Varification Functionalities Done

--- index.js
+++ index.js
@@ -30,6 +30,7 @@ const verifyToken = (req, res, next) => {
     return res.status(401).send({ message: "Unauthorized Access" });
   }
 
+  // Verify The Token
   jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
     if (err) {
       return res.status(401).send({ message: "Unauthorized Access" });
