326d37aa3da9bdffceaff21a353420aef9267444
Sheikh Jabed <jabedbd2295@gmail.com>
2025-02-26 23:48:08 +0600

    New Post Operation Added for Server Side

--- index.js
+++ index.js
@@ -48,6 +48,8 @@ const verifyToken = (req, res, next) => {
 
 
 
+
+
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.baizo.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
